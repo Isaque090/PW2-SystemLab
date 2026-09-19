c3e891997cd20497c8e306e4c6921c6c19b7f7ee
Isaque <124292329+Isaque090@users.noreply.github.com>
2026-09-18 22:26:59 -0300

    Add files via upload

---
+++ bd.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Máquina: localhost
+-- Data de Criação: 19-Set-2026 às 01:26
+-- Versão do servidor: 5.6.13
+-- versão do PHP: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de Dados: `bd`
+--
+CREATE DATABASE IF NOT EXISTS `bd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `bd`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lab`
+--
+
+CREATE TABLE IF NOT EXISTS `lab` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `numero_lab` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `numero_lab` (`numero_lab`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `professores`
+--
+
+CREATE TABLE IF NOT EXISTS `professores` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nm_professor` varchar(200) NOT NULL,
+  `ds_email` varchar(200) NOT NULL,
+  `rm` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ds_email` (`ds_email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `professores`
+--
+
+INSERT INTO `professores` (`id`, `nm_professor`, `ds_email`, `rm`) VALUES
+(1, 'aaaaaa', 'a@gmail.com', 11111);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turma`
+--
+
+CREATE TABLE IF NOT EXISTS `turma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ds_curso` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
