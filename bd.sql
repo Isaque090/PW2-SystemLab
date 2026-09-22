cd31739d6729e198308538a10338906059d79b8b
Isaque <124292329+Isaque090@users.noreply.github.com>
2026-09-22 10:54:46 -0300

    Refactor SQL dump to create database and tables

--- bd.sql
+++ bd.sql
@@ -1,94 +1,42 @@
--- phpMyAdmin SQL Dump
--- version 4.0.4.2
--- http://www.phpmyadmin.net
---
--- Máquina: localhost
--- Data de Criação: 22-Set-2026 às 00:57
--- Versão do servidor: 5.6.13
--- versão do PHP: 5.4.17
+CREATE DATABASE bd;
+USE bd;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE professores (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nm_professor VARCHAR(200) NOT NULL,
+    ds_email VARCHAR(200) NOT NULL UNIQUE,
+    rm INT NOT NULL
+
+   
+);
+
+CREATE TABLE turma (
+    id INT NOT NULL AUTO_INCREMENT  PRIMARY KEY,
+    ds_curso VARCHAR(200) NOT NULL
+
+  
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+CREATE TABLE lab (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    numero_lab INT NOT NULL UNIQUE,
+    status enum('Liberado','Reservado') NOT NULL DEFAULT 'Liberado'
 
---
--- Base de Dados: `bd`
---
-CREATE DATABASE IF NOT EXISTS `bd` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `bd`;
+   
+) ;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `lab`
---
+CREATE TABLE reservas (
+    id INT NOT NULL AUTO_INCREMENT   PRIMARY KEY ,
+    cd_professor INT NOT NULL,
+    cd_turma INT NOT NULL,
+    cd_lab INT NOT NULL,
+    horario_inicio DATETIME NOT NULL,
+    horario_termino DATETIME NOT NULL,
+    
+    FOREIGN KEY (cd_professor) REFERENCES professores(id),
+    FOREIGN KEY (cd_turma) REFERENCES turma(id),
+    FOREIGN KEY (cd_lab) REFERENCES lab(id)
 
-CREATE TABLE IF NOT EXISTS `lab` (
-  `id` int(11) NOT NULL DEFAULT '0',
-  `numero_lab` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `numero_lab` (`numero_lab`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `lab`
---
-
-INSERT INTO `lab` (`id`, `numero_lab`) VALUES
-(0, 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `professores`
---
-
-CREATE TABLE IF NOT EXISTS `professores` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nm_professor` varchar(200) NOT NULL,
-  `ds_email` varchar(200) NOT NULL,
-  `rm` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `ds_email` (`ds_email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Extraindo dados da tabela `professores`
---
-
-INSERT INTO `professores` (`id`, `nm_professor`, `ds_email`, `rm`) VALUES
-(1, 'aaaaaa', 'a@gmail.com', 11111);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `reservas`
---
-
-CREATE TABLE IF NOT EXISTS `reservas` (
-  `id` int(11) NOT NULL,
-  `cd_professor` int(11) NOT NULL,
-  `cd_lab` int(11) NOT NULL,
-  KEY `cd_professor` (`cd_professor`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `turma`
---
-
-CREATE TABLE IF NOT EXISTS `turma` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ds_curso` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+) ;
